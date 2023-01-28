67cad8f3e698824deb3b002ad0e2be7a8ef468e0
HotsykVictor <vitia2372087@gmail.com>
2023-01-28 20:56:42 +0200

    remove logs and refactored handlers

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,20 +3,13 @@ import styles from "src/components/TodoItem.module.css";
 const TodoItem = ({ item, onDeleteTask, onEditTask, onComplete }) => {
   const [isEditing, setIsEditing] = useState(false);
 
-  const deleteTaskHandler = () => {
-    onDeleteTask(item.id);
-  };
-  const completeHandler = () => {
-    onComplete(item.id);
-  };
   const editTaskHandler = (e) => {
     setIsEditing((prev) => !prev);
     onEditTask(e, item.id);
   };
-  console.log(item.text.length);
   return (
     <li>
-      {isEditing === true ? (
+      {isEditing ? (
         <textarea
           rows={Math.round(item.text.length / 40) || 1}
           onBlur={editTaskHandler}
@@ -27,13 +20,13 @@ const TodoItem = ({ item, onDeleteTask, onEditTask, onComplete }) => {
         <p
           className={item.isCompleted ? styles.completed : ""}
           onDoubleClick={editTaskHandler}
-          onClick={completeHandler}
+          onClick={() => onComplete(item.id)}
         >
           {item.text}
         </p>
       )}
 
-      <button className={styles.button} onClick={deleteTaskHandler}>
+      <button className={styles.button} onClick={() => onDeleteTask(item.id)}>
         Delete
       </button>
     </li>
